c24a13ab9f3c98786c257dd0cd6520eef86ba68e
wanwiset25 <wanwiset25@gmail.com>
2026-01-20 06:32:20 +0700

    add dashboard admin api

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   port: process.env.PORT || 2000,
   wsSecret: process.env.WS_SECRET || 'xinfin_xdpos_hybrid_network_stats',
+  adminSecret: process.env.ADMIN_SECRET || '',
   logger: {
     verbosity: process.env.VERBOSITY? parseInt(process.env.VERBOSITY) : 1
   },
@@ -8,7 +9,7 @@ const config = {
     url: `mongodb://${process.env.MONGODBURL || 'localhost:27017'}/forensics`
   },
   enableForensics: process.env.ENABLE_FORENSICS == 'true' ? true: false,
-  masternodeUrl: process.env.MASTERNODE_URL || "https://master.xinfin.network/api"
+  masternodeUrl: process.env.MASTERNODE_URL || "https://master.xinfin.network/api",
 };
 
 module.exports = {config}
